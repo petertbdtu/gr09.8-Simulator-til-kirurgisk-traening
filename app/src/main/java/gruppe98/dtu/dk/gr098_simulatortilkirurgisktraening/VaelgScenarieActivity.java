c8806c8f05cf43f7c1273c56d51035269d6048ab
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2018-10-28 21:12:52 +0100

    Tilføjet dummy-aktivitet til opretScenarie (struktur skal designes) + onClickListener
    Signed-off-by: hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>

--- app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgScenarieActivity.java
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgScenarieActivity.java
@@ -1,16 +1,30 @@
 package gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.Button;
 
-public class VaelgScenarieActivity extends AppCompatActivity {
+public class VaelgScenarieActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button opretScenarie;
+    Button redigerScenarie;
+    Button sletScenarie;
     LinearLayoutManager manageScenarier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaelg_scenarie);
+
+        opretScenarie = findViewById(R.id.nyt_scenarie);
+        opretScenarie.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, OpretScenarie.class));
     }
 }
