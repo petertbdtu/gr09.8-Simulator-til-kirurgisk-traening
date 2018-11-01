1c3243e02e60e3c71d810be4a5566ec52a048934
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2018-11-01 10:07:23 +0100

    Tilføjet recyclerview til visning af scenarieliste (placeholder liste)
    Signed-off-by: hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>

--- app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgScenarieActivity.java
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgScenarieActivity.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class VaelgScenarieActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button opretScenarie;
     Button redigerScenarie;
     Button sletScenarie;
-    LinearLayoutManager manageScenarier;
+
+    private ArrayList liste_scenarie_navne = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,58 @@ public class VaelgScenarieActivity extends AppCompatActivity implements View.OnC
 
         opretScenarie = findViewById(R.id.nyt_scenarie);
         opretScenarie.setOnClickListener(this);
+
+        indlaesScenarier();
+
+    }
+
+    private void indlaesScenarier() {
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+        liste_scenarie_navne.add("placeholder 1");
+        liste_scenarie_navne.add("placeholder 2");
+        liste_scenarie_navne.add("placeholder 3");
+
+
+
+        aktiverRecyclerView();
     }
 
     @Override
     public void onClick(View v) {
         startActivity(new Intent(this, OpretScenarie.class));
+    }
+
+    private void aktiverRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        VaelgScenarieRecyclerViewAdapter adapter = new VaelgScenarieRecyclerViewAdapter(liste_scenarie_navne, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgScenarieRecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class VaelgScenarieRecyclerViewAdapter extends RecyclerView.Adapter<VaelgScenarieRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<String> list_scenarie_navne = new ArrayList<>();
+    private Context context;
+
+    public VaelgScenarieRecyclerViewAdapter(ArrayList<String> list_scenarie_navne, Context context){
+        this.list_scenarie_navne = list_scenarie_navne;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.scenarie_liste_layout, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.scenarie_navn.setText(list_scenarie_navne.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list_scenarie_navne.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView scenarie_navn;
+        Button rediger;
+        Button slet;
+        ConstraintLayout liste_layout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            scenarie_navn = itemView.findViewById(R.id.scenarie_navn);
+            rediger = itemView.findViewById(R.id.knap_rediger_scenarie);
+            slet = itemView.findViewById(R.id.knap_slet_scenarie);
+            liste_layout = itemView.findViewById(R.id.listeElement);
+
+
+        }
+    } {
+
+    }
+}
