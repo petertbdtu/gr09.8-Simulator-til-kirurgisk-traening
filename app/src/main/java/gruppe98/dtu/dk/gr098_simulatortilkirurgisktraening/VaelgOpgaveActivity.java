331869c3465a6578e1e3751de1db06c78d59f9ed
Mette <s172840>
2018-11-01 16:59:28 +0100

    logik til valegOpgave.

--- app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgOpgaveActivity.java
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/VaelgOpgaveActivity.java
@@ -1,13 +1,42 @@
 package gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class VaelgOpgaveActivity extends AppCompatActivity {
+
+public class VaelgOpgaveActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ConstraintLayout clBrugssc;
+    ConstraintLayout clForbindTablet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaelg_opgave);
+
+        clBrugssc = findViewById(R.id.clBrugsscenairer);
+        clBrugssc.setOnClickListener(this);
+
+        clForbindTablet = findViewById(R.id.clForbinTTablet);
+        clForbindTablet.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.clBrugsscenairer:
+                Intent BrugssecenairerVisning = new Intent(getApplicationContext(), VaelgScenarieActivity.class);
+                startActivity(BrugssecenairerVisning);
+                break;
+            case R.id.clForbinTTablet:
+                Intent ForbinTTabletVisning = new Intent(getApplicationContext(), VaelgOpgaveActivity.class);
+                startActivity(ForbinTTabletVisning);
+                break;
+            default:
+                break;
+        }
     }
 }
