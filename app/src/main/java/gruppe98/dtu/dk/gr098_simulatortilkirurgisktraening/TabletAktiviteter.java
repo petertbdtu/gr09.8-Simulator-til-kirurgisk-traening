ecf45b5eade5d4da9dcda7d6cff86a5515ff23da
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2018-11-15 12:56:01 +0100

    Tilføjelse af placeholderaktivitet

---
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/TabletAktiviteter.java
@@ -0,0 +1,13 @@
+package gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabletAktiviteter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablet_aktiviteter);
+    }
+}
