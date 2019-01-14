cac4d77da11f87c7d4061466bc064368dc8d6c24
Mette <s172840>
2019-01-14 16:25:39 +0100

    sendbrugsscenarieRecyklerView + send_brugs.xml

--- app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/adapters/VaelgTabletsRecyleViewAdapter.java
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/adapters/VaelgTabletsRecyleViewAdapter.java
@@ -2,8 +2,6 @@ package gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
