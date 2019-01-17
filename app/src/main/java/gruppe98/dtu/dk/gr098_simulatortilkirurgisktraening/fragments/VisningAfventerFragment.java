f6b3c00c12696f7acab696788ba3c1816180cca8
petertbdtu <s175210@student.dtu.dk>
2019-01-17 13:49:03 +0100

    Tilføj plads til visning af device name under visningafventer
    Signed-off-by: petertbdtu <s175210@student.dtu.dk>

--- app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/fragments/VisningAfventerFragment.java
+++ app/src/main/java/gruppe98/dtu/dk/gr098_simulatortilkirurgisktraening/fragments/VisningAfventerFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import gruppe98.dtu.dk.gr098_simulatortilkirurgisktraening.R;
 
@@ -12,6 +13,12 @@ public class VisningAfventerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_visning_afventer, container, false);
+        View v = inflater.inflate(R.layout.fragment_visning_afventer, container, false);
+        Bundle args = getArguments();
+
+        TextView deviceNavn = v.findViewById(R.id.deviceNavnTextView);
+        deviceNavn.setText(args.getString("deviceNavn"));
+
+        return v;
     }
 }
